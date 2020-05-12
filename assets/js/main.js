ff59ba455d9dc009bbdb3641f30fcb24daaeac13
mahmoud <mahmoudradwan2625@gmail.com>
2020-05-12 04:35:25 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('open-menu').addEventListener('click', function() {
+     document.getElementById('menu').style.display = 'block'
+})
+document.getElementById('close-menu').addEventListener('click', function() {
+      document.getElementById('menu').style.display = 'none'
+ })
